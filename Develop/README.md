466ba711c4bc2515bb28c8de07b251696ef3c569
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2022-12-20 12:09:04 -0400

    add text body and link

---
+++ Develop/README.md
@@ -0,0 +1,42 @@
+# Professional Readme Generator <Your-Project-Title>
+
+   ## Description
+   
+   When user run app, the app will prompt with questions.
+   
+   ## Table of Contents
+   
+   - [Installation](#installation)
+   - [Usage](#usage)
+   - [Contributing](#contributing)
+   - [License](#license)
+   - [Tests](#tests)
+   - [Questions](#questions)
+
+   ## Installation
+   
+   To run application need to install necessary dependencies and please run the following commands.
+   
+   npm i
+   
+   ## Usage
+
+   nothing
+
+   ## Contributing
+
+   nothing
+   
+   ## License
+
+   This project is licensed under the MIT License.
+    
+   ## Tests
+   
+   To run the test, run the following command
+   node index.js
+   
+   ## Questions
+
+   If you have any questions about the repo, open an issue or contact me directly at [yzawa@gmail.com]. You can find my works at [yelzaw](https://github.com/yelzaw).
+   
--- Develop/index.js
+++ Develop/index.js
@@ -5,70 +5,114 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const generateREADME = ({github, email, project, description, license, install, runtest, usage, contribution}) =>{
    const contentReadme = 
-   `user github is ${github}\n, user email is ${email}\n, The project name is ${project}\n, The description${description},\n ${license},\n ${install},\n ${runtest},\n ${usage},\n ${contribution}`;
+   `# ${project} <Your-Project-Title>
+
+   ## Description
+   
+   ${description}
+   
+   ## Table of Contents
+   
+   - [Installation](#installation)
+   - [Usage](#usage)
+   - [Contributing](#contributing)
+   - [License](#license)
+   - [Tests](#tests)
+   - [Questions](#questions)
+
+   ## Installation
+   
+   To run application need to install necessary dependencies and please run the following commands.
+   
+   ${install}
+   
+   ## Usage
+
+   ${usage}
+
+   ## Contributing
+
+   ${contribution}
+   
+   ## License
+
+   This project is licensed under the ${license}.
+    
+   ## Tests
+   
+   To run the test, run the following command
+   ${runtest}
+   
+   ## Questions
+
+   If you have any questions about the repo, open an issue or contact me directly at [${email}]. You can find my works at [${github}](https://github.com/${github}).
+   `
+
+
    return contentReadme;
   }
 
-inquirer
-    .prompt([
-        {
-           type: 'input',
-           name: 'github',
-           message: 'What is your GitHub username?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?',
-         },
-         {
-            type: 'input',
-            name: 'project',
-            message: "What is your project's name?",
-         },
-         {
-            type: 'input',
-            name: 'description',
-            message: 'Please write a short description of your project',
-         },
-         {
-            type: 'list',
-            name: 'license',
-            message: 'What kind of license should your project have?',
-            choices: ['Apache License 2.0', 'MIT License', 'GNU General Public License 3.0', 'Eclipse Public License 2.0'],
-         },
-         {
-            type: 'input',
-            name: 'install',
-            message: 'What command should be run to install depedencies?',
-         },
-         {
-            type: 'input',
-            name: 'runtest',
-            message: 'What command should be run to tun tests?',
-         },
-         {
-            type: 'input',
-            name: 'usage',
-            message: 'What is does the user need to know about using the repo?',
-         },
-         {
-            type: 'input',
-            name: 'contribution',
-            message: 'What is does the user need to know about contributing to the repo?',
-         },
-    ])
-    .then((answers) => {
-        // console.log(generateREADME(answers));
-        fs.writeFile('readme.md', generateREADME(answers), (err) =>
-        err ? console.log(err) : console.log('Generating README.MD ....')
-    );
-    })
 // TODO: Create a function to write README file
-
+// function writeToFile(fileName, data) {
+// }
 
 // // TODO: Create a function to initialize app
-// function init() {}
+function init() {
+   inquirer
+   .prompt([
+      {
+         type: 'input',
+         name: 'github',
+         message: 'What is your GitHub username?',
+      },
+      {
+         type: 'input',
+         name: 'email',
+         message: 'What is your email address?',
+      },
+      {
+         type: 'input',
+         name: 'project',
+         message: "What is your project's name?",
+      },
+      {
+         type: 'input',
+         name: 'description',
+         message: 'Please write a short description of your project',
+      },
+      {
+         type: 'list',
+         name: 'license',
+         message: 'What kind of license should your project have?',
+         choices: ['Apache License 2.0', 'MIT License', 'GNU General Public License 3.0', 'Eclipse Public License 2.0'],
+      },
+      {
+         type: 'input',
+         name: 'install',
+         message: 'What command should be run to install depedencies?',
+      },
+      {
+         type: 'input',
+         name: 'runtest',
+         message: 'What command should be run to tun tests?',
+      },
+      {
+         type: 'input',
+         name: 'usage',
+         message: 'What is does the user need to know about using the repo?',
+      },
+      {
+         type: 'input',
+         name: 'contribution',
+         message: 'What is does the user need to know about contributing to the repo?',
+      },
+   ])
+   .then((answers) => {
+      fs.writeFile('README.md', generateREADME(answers), (err) =>
+      err ? console.log(err) : console.log('Generating README.MD ....')
+      );
+   })
+}
 
 // // Function call to initialize app
-// init();
+init();
